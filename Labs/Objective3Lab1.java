d2e70bac48828fa6088279e12bc5b11b599c2fd9
Mary Hudner <Mhudner@yahoo.com>
2024-01-16 16:01:01 -0700

    Finish Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1 = 10, num2 = 35, num3 = 71, sum = num1 + num2 + num3, average = sum/3;
+		System.out.println(average);
+
+
+
+	}
+}
