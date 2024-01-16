a35a33d4dca628d84e32028009fb90dc3f5848e1
Mary Hudner <Mhudner@yahoo.com>
2024-01-16 15:03:03 -0700

    Finish Objective2Lab2

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1988;
+		String month = "March";
+		String weekend = "False";
+
+		System.out.println(year + "\n" + month + "\n" + weekend);
+	}
+}
