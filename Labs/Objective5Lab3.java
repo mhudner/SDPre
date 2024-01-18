7e3bff4ab213100448b6b64572f0cbad53e528e1
Mary Hudner <Mhudner@yahoo.com>
2024-01-18 10:00:17 -0700

    Finish Objective5Lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+	public static void main(String[] args) {
+		
+			int userNum;
+
+			System.out.println("Please enter a number: ");
+			Scanner keyboard = new Scanner(System.in);
+			userNum = keyboard.nextInt();
+
+			if (userNum > 0) {
+				System.out.println(userNum + " is greater than " + 0);
+			}
+
+			else if (userNum < 0) {
+				System.out.println(userNum + " is less than " + 0);
+			}
+
+			else  {
+				System.out.println(userNum + " is equal to " + 0);
+			}
+
+			keyboard.close();
+
+	}
+}
