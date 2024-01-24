2414417c1eea0507d9ca9bd592427f1b91badb6c
Mary Hudner <Mhudner@yahoo.com>
2024-01-23 18:07:58 -0700

    Finish

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+
+		for (int a = 1; a <= 10; a ++) {
+			System.out.println(a);
+		}
+
+		for (int b = 10; b >= 1; b --) {
+			System.out.println(b);
+		}
+
+	}
+}
---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,13 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+
+		int sum = 0, count = 1;
+
+
+		for (count = 1; count <= 20; count ++) { //count = count + 1
+			sum += count; //sum = sum + count
+		}
+
+		System.out.println(sum);
+	}
+}
